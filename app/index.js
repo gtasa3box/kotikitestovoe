ed8c2d4fdf3ecc369cc789670d18e49ac6c316ab
gtasa3box <gtasa3box@gmail.com>
2021-05-19 15:48:24 +0300

    final commit

--- app/index.js
+++ app/index.js
@@ -2,6 +2,35 @@ import './index.scss';
 
 
 
+window.onload= function() {
+        document.getElementById('burgerlink').onclick = function() {
+        toogleclass(this);
+        opennav('nav', this);
+        return false;
+    };
+};
+
+function opennav(id, click_burger) {
+    const div = document.getElementById(id);
+    if(div.style.display == 'flex') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'flex';
+    }
+};
+
+function toogleclass(click_burger) {
+	if (click_burger.classList.contains('active')) {
+		click_burger.classList.remove('active');
+	}
+	else {
+		click_burger.classList.add('active');
+	}
+};
+
+
+
 
 
 console.log('Boilerplate is working!');
